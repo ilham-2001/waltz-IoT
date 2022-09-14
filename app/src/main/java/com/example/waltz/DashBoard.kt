bc48dd7f14c7a018718a7e617f54f5c9d753ede5
irizqy <arizqyakbar@gmail.com>
2022-09-14 20:02:48 +0700

    add dashboard

--- app/src/main/java/com/example/waltz/DashBoard.kt
+++ app/src/main/java/com/example/waltz/DashBoard.kt
@@ -1,16 +1,29 @@
 package com.example.waltz
 
 import android.content.Context
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
 import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.*
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.collections.ArrayList
 
 class DashBoard : AppCompatActivity() {
 
     private lateinit var mqttClient: MqttAndroidClient
+    private lateinit var kwhChart: LineChart
 
     companion object {
         const val TAG = "AndroidMqttClient"
@@ -25,6 +38,10 @@ class DashBoard : AppCompatActivity() {
 
         val clientID = MqttClient.generateClientId()
         connect(this, clientID)
+
+        // dummy test data
+        kwhChart = findViewById(R.id.kwh_chart)
+        setLineChart()
     }
 
     private fun connect(context: Context, clientID: String) {
@@ -50,5 +67,79 @@ class DashBoard : AppCompatActivity() {
         } catch (e: MqttException) {
             Log.d(TAG, "Error when trying to connect")
         }
+    }
+
+    private fun setLineChart() {
+        val linevalues = ArrayList<Entry>()
+        linevalues.add(Entry(1f, 120_000F))
+        linevalues.add(Entry(2f, 100_000F))
+        linevalues.add(Entry(3f, 400_000F))
+        linevalues.add(Entry(4f, 375_000F))
+        linevalues.add(Entry(4f, 230_000F))
+        linevalues.add(Entry(4f, 143_500F))
+        linevalues.add(Entry(4f, 105_7500F))
+        linevalues.add(Entry(5f, 278_4560F))
+        linevalues.add(Entry(6f, 122_500F))
+        linevalues.add(Entry(7f, 225_900F))
+        linevalues.add(Entry(8f, 195_000F))
+        linevalues.add(Entry(9f, 165_980F))
+        linevalues.add(Entry(10f, 215_000F))
+        linevalues.add(Entry(11f, 267_500F))
+        linevalues.add(Entry(12f, 333_000f))
+
+        val linedataset = LineDataSet(linevalues, "First")
+        //We add features to our chart
+        linedataset.color = resources.getColor(R.color.black)
+//        linedataset.color = resources.getColor()
+
+        linedataset.circleRadius = 2f
+        linedataset.setDrawFilled(true)
+        linedataset.valueTextSize = 10f
+        linedataset.fillColor = resources.getColor(R.color.main)
+        linedataset.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+
+        val data = LineData(linedataset)
+        kwhChart.data = data
+        kwhChart.setBackgroundColor(resources.getColor(R.color.white))
+        kwhChart.animateXY(2000, 2000, Easing.EaseInCubic)
+
+        val xAxis = kwhChart.xAxis
+        xAxis.granularity = 1f
+        xAxis.valueFormatter = (object : ValueFormatter() {
+            private val pattern = "dd MMM yy"
+            private val mFormat = SimpleDateFormat(pattern)
+            private val inputFormat = SimpleDateFormat("yyyy-MM-dd")
+
+            private fun formatValue(value: Float): String {
+                val month = when (value) {
+                    1f -> "Jan"
+                    2f -> "Feb"
+                    3f -> "Mar"
+                    4f -> "Apr"
+                    5f -> "May"
+                    6f -> "Jun"
+                    7f -> "Jul"
+                    8f -> "Aug"
+                    9f -> "Sep"
+                    10f -> "Oct"
+                    11f -> "Nov"
+                    12f -> "Dec"
+                    else -> ""
+                }
+
+                return month
+            }
+
+            override fun getFormattedValue(value: Float): String {
+//                val millis = TimeUnit.HOURS.toMillis(value.toLong())
+//                return mFormat.format(inputFormat.parse(xLabel[value.toInt()]))
+//                val date = Date(value.toLong())
+
+//                Log.d(TAG, value.toLong().toString())
+                Log.d(TAG, value.toInt().toString())
+
+                return formatValue(value)
+            }
+        })
     }
 }
--- app/src/main/java/com/example/waltz/MainActivity.kt
+++ app/src/main/java/com/example/waltz/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("DEPRECATION")
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+//        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
         val actionBar = supportActionBar
