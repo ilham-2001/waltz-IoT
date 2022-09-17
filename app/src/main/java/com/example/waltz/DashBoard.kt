c3e4f12f94dd0da951995464f4e8a647f7db3005
irizqy <arizqyakbar@gmail.com>
2022-09-17 11:01:10 +0700

    improve dashboard

--- app/src/main/java/com/example/waltz/DashBoard.kt
+++ app/src/main/java/com/example/waltz/DashBoard.kt
@@ -7,10 +7,9 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.ValueFormatter
 import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.*
@@ -24,6 +23,7 @@ class DashBoard : AppCompatActivity() {
 
     private lateinit var mqttClient: MqttAndroidClient
     private lateinit var kwhChart: LineChart
+    private lateinit var monthlyCost: BarChart
 
     companion object {
         const val TAG = "AndroidMqttClient"
@@ -33,15 +33,17 @@ class DashBoard : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash_board)
 
-        val actionBar = supportActionBar
-        actionBar!!.hide()
+//        val actionBar = supportActionBar
+//        actionBar!!.hide()
 
         val clientID = MqttClient.generateClientId()
         connect(this, clientID)
 
         // dummy test data
         kwhChart = findViewById(R.id.kwh_chart)
+        monthlyCost = findViewById(R.id.cost_monthly_chart)
         setLineChart()
+        setBarChart()
     }
 
     private fun connect(context: Context, clientID: String) {
@@ -49,7 +51,7 @@ class DashBoard : AppCompatActivity() {
         Log.d(TAG, context.toString())
         mqttClient = MqttAndroidClient(context, serverURI, clientID)
 
-        Log.d(TAG, "trying to connect")
+//        Log.d(TAG, "trying to connect")
 
         try {
             val token = mqttClient.connect()
@@ -69,23 +71,58 @@ class DashBoard : AppCompatActivity() {
         }
     }
 
+    private fun formatValueMonth(value: Float): String {
+        val month = when (value) {
+            1f -> "Jan"
+            2f -> "Feb"
+            3f -> "Mar"
+            4f -> "Apr"
+            5f -> "May"
+            6f -> "Jun"
+            7f -> "Jul"
+            8f -> "Aug"
+            9f -> "Sep"
+            10f -> "Oct"
+            11f -> "Nov"
+            12f -> "Dec"
+            else -> ""
+        }
+
+        return month
+    }
+
+    private fun formatValueHour(value: Float): String {
+        val month = when (value) {
+            1f -> "00"
+            2f -> "01"
+            3f -> "02"
+            4f -> "03"
+            5f -> "04"
+            6f -> "05"
+            7f -> "06"
+            8f -> "07"
+            9f -> "08"
+            10f -> "09"
+            11f -> "10"
+            12f -> "11"
+            else -> ""
+        }
+
+        return month    }
+
     private fun setLineChart() {
         val linevalues = ArrayList<Entry>()
-        linevalues.add(Entry(1f, 120_000F))
-        linevalues.add(Entry(2f, 100_000F))
-        linevalues.add(Entry(3f, 400_000F))
-        linevalues.add(Entry(4f, 375_000F))
-        linevalues.add(Entry(4f, 230_000F))
-        linevalues.add(Entry(4f, 143_500F))
-        linevalues.add(Entry(4f, 105_7500F))
-        linevalues.add(Entry(5f, 278_4560F))
-        linevalues.add(Entry(6f, 122_500F))
-        linevalues.add(Entry(7f, 225_900F))
-        linevalues.add(Entry(8f, 195_000F))
-        linevalues.add(Entry(9f, 165_980F))
-        linevalues.add(Entry(10f, 215_000F))
-        linevalues.add(Entry(11f, 267_500F))
-        linevalues.add(Entry(12f, 333_000f))
+        linevalues.add(Entry(1f, 580F))
+        linevalues.add(Entry(2f, 560F))
+        linevalues.add(Entry(3f, 650F))
+        linevalues.add(Entry(4f, 700F))
+        linevalues.add(Entry(5f, 740F))
+        linevalues.add(Entry(6f, 790F))
+        linevalues.add(Entry(7f, 840F))
+        linevalues.add(Entry(8f, 880F))
+        linevalues.add(Entry(9f, 930F))
+        linevalues.add(Entry(10f, 950F))
+        linevalues.add(Entry(11f, 1_012F))
 
         val linedataset = LineDataSet(linevalues, "First")
         //We add features to our chart
@@ -110,35 +147,47 @@ class DashBoard : AppCompatActivity() {
             private val mFormat = SimpleDateFormat(pattern)
             private val inputFormat = SimpleDateFormat("yyyy-MM-dd")
 
-            private fun formatValue(value: Float): String {
-                val month = when (value) {
-                    1f -> "Jan"
-                    2f -> "Feb"
-                    3f -> "Mar"
-                    4f -> "Apr"
-                    5f -> "May"
-                    6f -> "Jun"
-                    7f -> "Jul"
-                    8f -> "Aug"
-                    9f -> "Sep"
-                    10f -> "Oct"
-                    11f -> "Nov"
-                    12f -> "Dec"
-                    else -> ""
-                }
-
-                return month
-            }
-
             override fun getFormattedValue(value: Float): String {
 //                val millis = TimeUnit.HOURS.toMillis(value.toLong())
 //                return mFormat.format(inputFormat.parse(xLabel[value.toInt()]))
 //                val date = Date(value.toLong())
 
 //                Log.d(TAG, value.toLong().toString())
-                Log.d(TAG, value.toInt().toString())
+//                Log.d(TAG, value.toInt().toString())
 
-                return formatValue(value)
+                return formatValueHour(value)
+            }
+        })
+    }
+
+    private fun setBarChart() {
+        val barValues = ArrayList<BarEntry>()
+        barValues.add(BarEntry(1f, 320_000f))
+        barValues.add(BarEntry(2f, 220_000f))
+        barValues.add(BarEntry(3f, 190_000f))
+        barValues.add(BarEntry(4f, 110_000f))
+        barValues.add(BarEntry(5f, 475_000f))
+        barValues.add(BarEntry(6f, 325_000f))
+        barValues.add(BarEntry(7f, 225_000f))
+        barValues.add(BarEntry(8f, 212_500f))
+        barValues.add(BarEntry(9f, 234_560f))
+        barValues.add(BarEntry(10f, 110_5000f))
+        barValues.add(BarEntry(11f, 123_456f))
+        barValues.add(BarEntry(12f, 122_500f))
+
+        val barDataSet = BarDataSet(barValues, "Monthly Cost")
+        val barData = BarData(barDataSet)
+        monthlyCost.data = barData
+
+        barDataSet.valueTextColor = R.color.black
+        barDataSet.setColor(resources.getColor(R.color.purple_200))
+        barDataSet.valueTextSize = 12f
+        monthlyCost.description.isEnabled = false
+
+        val xAxis = monthlyCost.xAxis
+        xAxis.valueFormatter = (object: ValueFormatter() {
+            override fun getFormattedValue(value: Float): String {
+                return formatValueMonth(value)
             }
         })
     }
